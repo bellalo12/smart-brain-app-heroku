439e3d3e394e580713197f6dee0aeb894d580c00
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-05-27 22:17:09 +0800

    deploy

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
 
   onButtonSubmit=()=>{
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl',{
+    fetch('https://morning-cove-74731.herokuapp.com/imageurl',{
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -76,7 +76,7 @@ class App extends Component {
     .then(res=> res.json())
       .then(res=>{
         if(res){
-          fetch('http://localhost:3000/image',{
+          fetch('https://morning-cove-74731.herokuapp.com/image',{
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
