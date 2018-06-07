3f71d9e3a1163fabbecedbc79972f623429ae4ce
Bella Lo <bellalo@BestaMacBook-Air.local>
2018-06-07 12:04:31 +0800

    fix submit button

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -82,7 +82,7 @@ class Register extends React.Component {
       onClick={this.onSubmitSignIn}
       className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
       type="submit"
-      value="Register"/>
+      value="submit"/>
     </div>
     </div>
    </main>
